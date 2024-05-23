6fe14f64a8abadd687cdf72540cad309043ac850
PlKurak <dawid.djpk8@gmail.com>
2024-05-24 00:03:29 +0200

    work

--- BetterSoundCloud/SoundSky/admin.py
+++ BetterSoundCloud/SoundSky/admin.py
@@ -4,4 +4,6 @@ from .models import *
 admin.site.register(Song)
 admin.site.register(Playlist)
 admin.site.register(Comment)
+admin.site.register(Followers)
+admin.site.register(Follows)
 # Register your models here.
