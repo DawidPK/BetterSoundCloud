86f148810f4b6500638260b9cc4329cd80c73406
PlKurak <dawid.djpk8@gmail.com>
2024-05-23 22:06:57 +0200

    Added register

--- BetterSoundCloud/SoundSky/forms.py
+++ BetterSoundCloud/SoundSky/forms.py
@@ -1,10 +1,18 @@
 from django import forms
-from .models import *
+# from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User  # Add this import statement
 
-class RegisterForm(forms.ModelForm):
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ["name", "email", "password"]
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+        fields = ["username", "email", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super(CreateUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
